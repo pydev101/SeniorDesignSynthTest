9576b6d54a7bf1adfd4a8fd8c0cd20bba29d2cb5
Carson E <30803947+pydev101@users.noreply.github.com>
2025-10-19 23:00:23 -0500

    Good Notes

--- BetterSynth/main.py
+++ BetterSynth/main.py
@@ -4,11 +4,11 @@ import sounddevice as sd
 import threading
 import matplotlib.pyplot as plt
 from scipy.fft import fft, fftfreq
-
+import keyboard
 
 # Parameters
 sample_rate = 44100  # samples per second
-block_size = 1024    # block size
+block_size = 1024  # block size
 
 # For FFT plotting
 fft_data = np.zeros(block_size)
@@ -23,24 +23,73 @@ ax.set_ylabel("Amplitude")
 plt.ion()
 
 phase = 0
-# a
+
+
+class Note:
+    # Attack (s)
+    # Decay (s)
+    # Sustain (amplitude)
+    # Release (s)
+    def __init__(self, freq, attack, decay, sustain, release):
+        self.freq = freq
+        self.A = attack
+        self.D = decay
+        self.S = sustain
+        self.R = release
+
+        self.amplitude = 0
+        self.startTime = None
+
+        self.envelope = np.vectorize(self.envelopeFunct)
+
+    def getClip(self, t):
+        if self.startTime is None:
+            self.startTime = t[0]
+        t = t - self.startTime
+
+        self.amplitude = self.envelope(t)
+        return np.multiply(self.amplitude, np.sin(2 * np.pi * self.freq * t))
+
+    def envelopeFunct(self, t):
+        aMax = 1
+        buttonDown = True
+
+        if buttonDown:
+            amp = t * (aMax/self.A) if t < self.A else 0
+            amp = aMax - ((t-self.A) * ((aMax-self.S) / self.D)) if self.A < t < self.A+self.D else amp
+            amp = self.S if self.A+self.D < t else amp
+        else:
+            pass
+
+        # amp = aMax - ((self.D - self.A) * (1 / self.D)) if self.A+self.D < t < self.A + self.D + self.S else amp
+        if amp < 0:
+            return 0
+        return amp
+
+notes = [Note(300, 1, 1, 0.5, 1), Note(432, 0.3, 8, 0.5, 1)]
+
 def audio_callback(outdata, frames, time, status):
     global phase, fft_data
 
     t = (np.arange(frames) + phase) / sample_rate
     wave = np.zeros_like(t)
 
-    tStart = phase / sample_rate # Start time of frame in s
-    print(tStart)
+    tStart = phase / sample_rate  # Start time of frame in s
 
-    wave += np.sin(2 * np.pi * 300 * t)*np.exp(-t/1)
-    wave += np.sin(2 * np.pi * 600 * t)*np.exp(-t/2)
+
+    for n in notes:
+        wave += n.getClip(t)
+    # if t[0] > 1.2:
+        # wave += notes[1].getClip(t)
+    # wave += np.sin(2 * np.pi * 300 * t) * np.exp(-t / 1)
+    # wave += np.sin(2 * np.pi * 600 * t) * np.exp(-t / 2)
 
     outdata[:] = wave.reshape(-1, 1)
 
     # Save block for FFT
     fft_data = wave.copy()
     phase += frames
+
 
 def update_plot():
     global fft_data
@@ -50,6 +99,7 @@ def update_plot():
     line.set_ydata(Y)
     ax.set_ylim(0, max(1, np.max(Y)))
     plt.pause(0.001)
+
 
 with sd.OutputStream(channels=1, callback=audio_callback, samplerate=sample_rate, blocksize=block_size):
     while True:
